7414eea7aad957cdadd416bcefb7b384d711a45a
KhoiDang1209 <khoibk123123@gmail.com>
2024-10-03 21:14:31 +0700

    update message

---
+++ home.php
@@ -0,0 +1,38 @@
+<?php
+include 'components/connect.php';
+echo create_unique_id();
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-Edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+        <link rel="stylesheet" href="css/style.css">
+
+    </head>
+    <body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="js/script.js"></script>
+    </body>
+</html>
